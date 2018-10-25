c6f4e4153259cfa9e8029150428e66cd8f047ada
Sai Sukrutha <14861646+Sukku1@users.noreply.github.com>
2018-10-25 19:24:03 +0530

    Part 1- Basic Preemptive Scheduler

---
+++ threads/hello.c
@@ -0,0 +1,10 @@
+//File Added by-Sukrutha
+//hello.c for testing
+
+#include "threads/hello.h"
+
+void say_hello(void)
+{
+    printf("*************Hello Pintos!****************\n");
+}
+
---
+++ threads/hello.h
@@ -0,0 +1,7 @@
+#ifndef THREADS_HELLO_H
+#define THREADS_HELLO_H
+#endif
+
+#include <stdio.h>
+
+void say_hello(void);
